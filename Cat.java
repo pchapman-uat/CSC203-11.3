e64207a4c35a5318968ade55b349ef7b241f0464
Preston Chapman <pchapman82070@uat.edu>
2024-03-30 16:35:05 -0700

    Comments

--- Cat.java
+++ Cat.java
@@ -1,15 +1,23 @@
+// Import the color class for the color of the labels.
 import java.awt.Color;
+// Import the JLabel class for use in functions and variables   
 import javax.swing.JLabel;
 
+// This is the cat class that will be used to create the cat object
+// Only one cat is made, however having it as a class makes things easier to manage
 public class Cat {
+    // Create variables for the stats of the cat
     public int hunger = 100;
     public int energy = 100;
     public int happiness = 100;
 
+    // Create variables for the labels for the stats
     private JLabel hungerLabel;
     private JLabel energyLabel;
     private JLabel happinessLabel;
 
+    // Create a constructor for the cat object
+    // Pass in the labels for the stats
     public Cat(JLabel hunger, JLabel energy, JLabel happiness) {
         System.out.println("Creating a new cat");
         this.hungerLabel = hunger;
@@ -17,6 +25,7 @@ public class Cat {
         this.happinessLabel = happiness;
     }
 
+    // Set the color of the label based on the value
     private void setColor(JLabel label, int value){
         if(value > 50){
             label.setForeground(Color.GREEN);
@@ -27,9 +36,12 @@ public class Cat {
         }
     }
 
+    // Cap the value between the max and min values
     private Integer capValue(int value, Integer max, Integer min){
+        // If the max and min values are null, set them to 100 and 0 respectively
         max = max != null ? max : 100;
         min = min != null ? min : 0;
+        // If the value is greater than the max, set it to the max value, otherwise if it is less than the min, set it to the min value
         if(value > max){
             value = max;
         } else if(value < min){
@@ -38,53 +50,74 @@ public class Cat {
         return value;
         
     }
+    // Update the labels
     private void updateLables(){
 
+        // Set the color of the labels based on the value
         this.setColor(energyLabel, energy);
         this.setColor(hungerLabel, hunger);
         this.setColor(happinessLabel, happiness);
 
+        // Cap the values between the max and min values
+        // Null is used to use the default values of 100, and 0
         this.energy = this.capValue(energy, null, null);
         this.hunger = this.capValue(hunger, null, null);
         this.happiness = this.capValue(happiness, null, null);
 
+        // Set the text of the labels
         this.energyLabel.setText("Energy: " + this.energy);
         this.hungerLabel.setText("Hunger: " + this.hunger);
         this.happinessLabel.setText("Happiness: " + this.happiness);
     }
+    // Feed the cat
     public void feed() {
         System.out.println("Feeding the cat");
+        // Add 10 to the hunger and energy
         this.hunger += 10;
         this.energy += 10;
+        // Update the labels
         this.updateLables();
     }
+    // Play with the cat
     public void play(JLabel hunger) {
         System.out.println("Playing with the cat");
+        // Subtract 5 from the hunger and 10 from the energy
+        // Add 10 to the happiness
         this.hunger -= 5;
-        this.energy -= 5;
+        this.energy -= 10;
         this.happiness += 10;
+        // Update the labels
         this.updateLables();
     }
 
+    // Reset the stats of the cat
     public void restart(){
+        // Set all stat values to 100
         this.hunger = 100;
         this.energy = 100;
         this.happiness = 100;
+        // Update the labels
         this.updateLables();
     }
 
+    // Create a loop that will update the stats of the cat every second
     public void loop(){
+        // Always run the loop
         while(true){
+            // Sleep for 1 second
+            // A try catch is used to catch any errors that may occur
             try {
                 Thread.sleep(1000);
-
-                if(Math.random() > 0.5) this.hunger -= 1;
-                if(Math.random() > 0.5) this.energy -= 1;
-                if(Math.random() > 0.5) this.happiness -= 1;
-                this.updateLables();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            // After the wait has finished, subtract 1 from the hunger, energy, and happiness
+            // A random number is generated to determine if the value is subtracted or not
+            if(Math.random() > 0.5) this.hunger -= 1;
+            if(Math.random() > 0.5) this.energy -= 1;
+            if(Math.random() > 0.5) this.happiness -= 1;
+            // Update the labels
+            this.updateLables(); 
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,72 +1,113 @@
+// Import the java color class for the color of the labels.
 import java.awt.Color;
+// Import the flow layout class for the layout of the labels.
 import java.awt.FlowLayout;
+// Import the image class for the image of the cat.
 import java.awt.Image;
-
+// Import swing for the UI
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Declare the variables for the frame size
         int frameWidth = 250;
         int frameHeight = 400;
+        // Create a new frame (window)
         JFrame frame = new JFrame();
+        // Set the frame size and close operation
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(frameWidth, frameHeight);
+        // Make the frame visible
         frame.setVisible(true);
-        frame.setBackground(Color.DARK_GRAY);
+        // Create lables for the title and subtitle
         JLabel title = new JLabel("Pet Simulator");
         JLabel subtitle = new JLabel("CSC203 11.3 | UAT");
         
-
+        // Create a panel for the header
+        // This is like a div in html
         JPanel header = new JPanel();
+        // Set the size of the panel
         header.setSize(frameWidth, 50);
+        // Add the title and subtitle to the panel
         header.add(title);
         header.add(subtitle);
-        header.setVisible(true);
+        // Add the panel to the frame
         frame.add(header);
+        // Set the layout of the frame
+        // Flow allows for an adaptive layout
+        // This is like flex in css
         frame.setLayout(new FlowLayout());
 
-
+        // Create a panel for the name
         JPanel name = new JPanel();
+        // Set the size of the panel
         name.setSize(frameWidth, 50);
+        // Set the background color of the panel
         name.setBackground(Color.GRAY);
+        // Set the layout of the panel
         name.setLayout(new FlowLayout());
+        // Create a text input area
         JTextArea textArea = new JTextArea();
+        // Set the size of the text area
         textArea.setSize(frameWidth, 50);
+        // Set the default text
         textArea.setText("Enter the cat's name");
+        // Add the name to the frame
         name.add(textArea);
 
+        // Add the name panel to the frame
         frame.add(name);
+
+        // Create a label for the cat image
         JLabel cat = new JLabel();
+        // Create an image icon from the file
         ImageIcon catIcon = new ImageIcon("assets/cat.jpg");
+        // Get the image data from the icon
         Image catImage = catIcon.getImage();
+        // Scale the image to the size of the label
         Image newCatImage = catImage.getScaledInstance(200, 200, Image.SCALE_REPLICATE);
+        // Set the icon to the resied image
         cat.setIcon(new ImageIcon(newCatImage));   
+        // Add the image to the frame
         frame.add(cat);
 
   
+        // Create a panel for the stats
         JPanel stats = new JPanel();
+        // Create stats labels
         JLabel hunger = new JLabel("Hunger: " + 100);
         JLabel energy = new JLabel("Energy: " + 100);
         JLabel happiness = new JLabel("Happiness" + 100);
+        // Set the background color of the panel
         stats.setBackground(Color.GRAY);
+        // Create a new cat object
+        // Pass in the labels for the stats
+        // This will be used to update the stats visualy
         Cat catObj = new Cat(hunger, energy, happiness);
+        // Add the labels to the stats pannel
         stats.add(hunger);
         stats.add(energy);
         stats.add(happiness);
+        // Set the size of the stats panel
         stats.setSize(frameWidth, 100);
+        // Set the layout of the stats panel
         stats.setLayout(new FlowLayout());
         frame.add(stats);
+
+        // Create buttons for Feeding, Playing, and Reseting
+        // Add action listener will runt the function when the button is clicked
         JButton feedButton = new JButton("Feed");
         feedButton.addActionListener(e -> catObj.feed());
         JButton playButton = new JButton("Play");
         playButton.addActionListener(e -> catObj.play(hunger));
-        frame.add(playButton);
-        frame.add(feedButton);
-
         JButton restartButton = new JButton("Restart");
         restartButton.addActionListener(e -> catObj.restart());
+
+        // Add the buttons to the frame
+        frame.add(playButton);
+        frame.add(feedButton);
         frame.add(restartButton);
+        // Run the looping function for the cat
         catObj.loop();
     }
 }
